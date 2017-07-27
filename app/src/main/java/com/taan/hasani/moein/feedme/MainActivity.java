d7405e6402010d43d813b89538e412adbaf86763
Moeinh77 <moeinhasani77@gmail.com>
2017-07-27 17:36:41 +0430

    1

--- app/src/main/java/com/taan/hasani/moein/feedme/MainActivity.java
+++ app/src/main/java/com/taan/hasani/moein/feedme/MainActivity.java
@@ -1,13 +1,86 @@
 package com.taan.hasani.moein.feedme;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.security.auth.login.LoginException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG ="TAG" ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+    //////////////////////
+    private class downloadData extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            String rss=downloadXML(params[0]);
+            if(rss.equals(""))
+                Log.e(TAG, "doInBackground: Error downloading xml");
+
+            return rss;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+
+            super.onPostExecute(s);
+        }
+
+    private String downloadXML (String urlpath){
+        StringBuilder stringBuilder=new StringBuilder();
+
+       try {
+           /////
+           URL url=new URL(urlpath);
+           HttpURLConnection connection=(HttpURLConnection)url.openConnection();
+           InputStream input=connection.getInputStream();
+           InputStreamReader inputreader=new InputStreamReader(input);
+           BufferedReader reader=new BufferedReader(inputreader);
+           ////
+           int num_of_read_chars;
+
+           char[] charbuffered = new char[500];
+
+           while (true) {
+               num_of_read_chars = reader.read(charbuffered);
+               if (num_of_read_chars < 0)
+                   break;
+               if (num_of_read_chars > 0)
+                   stringBuilder.append(String.copyValueOf(charbuffered, 0, num_of_read_chars));
+           }
+           reader.close();
+
+       }catch (MalformedURLException e){
+           Log.e(TAG, "downloadXML: MalformedException"+e.getMessage());
+       }catch (IOException e){
+           Log.e(TAG,"downloadXML: IO Exception reading"+e.getMessage());
+       }
+        return stringBuilder.toString();
+
+    }
+
+
+    }
+    //////////////////////
+
+
 }
+
